b1f57cd5265bb72e25a53c150dfee72f3e057797
Ashonst2844 <agusyantosugiyanto@gmail.com>
2023-10-08 13:43:52 +0700

    Add Veronica Photo

--- jquery.js
+++ jquery.js
@@ -1,5 +1,5 @@
 // If Character Photo Is Hover, Short Biodata Will Be Shown
-for (let i = 0; i <= 8; i++) {
+for (let i = 0; i <= 9; i++) {
     $("img.image" + i).hover(function () {
         $("p.caption" + i).fadeToggle("fast")
     });
